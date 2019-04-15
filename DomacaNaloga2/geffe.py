3c43e7fbb2277135c1a00ba0c36a556b2875f783
Benjamin Benčina <benjamin.bencina@gmail.com>
2019-04-15 14:14:53 +0200

    Memory-heavy version

--- DomacaNaloga2/geffe.py
+++ DomacaNaloga2/geffe.py
@@ -212,6 +212,19 @@ def Geffe(x1, x2, x3):
                 zap += str(bit % 2)
         return zap
 
+# NOTES:
+# - The main difference between the memory-heavy and time-heavy version is
+#   that the memory heavy version generates all lfsr sequences  prior to
+#   the correlation attack but then only searches through them using
+#   a native hash map implementation (dictionary) whereas the time-heavy
+#   version generates sequences of necesarry length during the attack.
+# - One of main consequences of this is that the memory-heavy version
+#   works fast on longer words (hence the sorting of the wordlist by
+#   length in descending order), yet works painfully slow on slow words.
+#   The time-heavy version works with about the same speed regardless
+#   of word length but is a bit slower on average.
+
+# time-heavy version
 def BreakGeffe(c, WLIST = WORDLIST):
         '''Performs a know-plaintext correlation attack on the Geffe generator.
                 Rolls every word in the wordlist through the ciphertext,
@@ -235,7 +248,7 @@ def BreakGeffe(c, WLIST = WORDLIST):
                 print('Testing word: ', w)
                 # rolling the word over the ciphertext
                 for i in range(len(c)//5 - n//5 + 1):
-                        print('.', end=' ')
+                        print('.', end='')
                         # key candidate at the current marker of length n
                         z = xor(b, c[i*5:n+i*5])
 
@@ -297,6 +310,102 @@ def BreakGeffe(c, WLIST = WORDLIST):
         print('DecipheringError: Something went wrong. Try updating the wordlist or lowering the tolerance.')
         return None
 
+# memory-heavy version
+def BreakGeffe2(c, WLIST = WORDLIST):
+        '''Performs a know-plaintext correlation attack on the Geffe generator.
+                Rolls every word in the wordlist through the ciphertext,
+                calculates the key candidate and uses the statistical weakness
+                of the generator function to calculate the key.
+                We notice the plaintext guess was wrong when turning bits to
+                letters leads to an indexation error or with the help of
+                frequency analysis.
+                If the wordlist contains only one word we know for sure is at the
+                very begining of the text, this function is equivalent to a basic
+                correlation attack. Thus as we know the word 'CRYPTOGRAPHY' is the
+                first word of our plaintext, one can substitute WORDLIST for
+                ['CRYPTOGRAPHY'] and yield positive results.'''
+        print('Generating lfsr sequences...')
+        lfsr1 = {}
+        lfsr2 = {}
+        lfsr3 = {}
+        for seed in allBitSeq(5):
+                x = LFSR_1(seed, len(c))
+                lfsr1[seed] = x
+        for seed in allBitSeq(7):
+                x = LFSR_2(seed, len(c))
+                lfsr2[seed] = x
+        for seed in allBitSeq(11):
+                x = LFSR_3(seed, len(c))
+                lfsr3[seed] = x
+        for w in WLIST:
+                b = wordToBits(w)
+                n = len(b)
+                # if the word is longer than the ciphertext then it's surely not
+                # in the text
+                if len(c) < n:
+                        continue
+                print('Testing word: ', w)
+                # rolling the word over the ciphertext
+                for i in range(len(c)//5 - n//5 + 1):
+                        print('.', end='')
+                        # key candidate at the current marker of length n
+                        z = xor(b, c[i*5:n+i*5])
+
+                        # correlation attack:
+                        # which x1 match z cca. 3/4 of the time?
+                        x1s = []
+                        for s in lfsr1:
+                                m_coef = matching(lfsr1[s][i*5:n+i*5], z)
+                ##                print("Testing1: ", seed, m_coef)
+                                if m_coef > 0.7:
+##                                        print('Success in LFSR1, seed: ', seed)
+                                        x1s.append(s)
+
+                        # which x3 match z cca. 3/4 of the time?
+                        x3s = []
+                        for s in lfsr3:
+                                m_coef = matching(lfsr3[s][i*5:n+i*5], z)
+                ##                print("Testing3: ", seed, m_coef)
+                                if m_coef > 0.74:
+##                                        print('Success in LFSR3, seed: ', seed)
+                                        x3s.append(s)
+
+                        # now for the final key piece: x2
+                        x2 = None
+                        for s in lfsr2:
+                                for s1 in x1s:
+                                        for s3 in x3s:
+                                                # make key candidate for current (x1, x2, x3)
+                                                z_cand = Geffe(lfsr1[s1], lfsr2[s], lfsr3[s3])
+                                                # does it match the guessed key?
+                                                if z_cand[i*5:n+i*5] == z:
+                                                        l = len(c)
+                                                        # make key for the whole ciphertext
+                                                        key = z_cand
+                                                        # decrypt with calculated key
+                                                        plaintext_bit = xor(c, key)
+                                                        # check for errors
+                                                        try:
+                                                                # does it transate to letters?
+                                                                # not all bit combinations are valid
+                                                                # as an alphabet index
+                                                                plaintext = bitsToWord(plaintext_bit)
+                                                                # if it succeded, check if the language
+                                                                # is English
+                                                                index = frequencyAnalysis(plaintext)
+                                                                if index > TOL:
+                                                                        continue
+                                                        except:
+                                                                continue
+                                                        # if all is well then return
+                                                        print('\nKey found: ', s1, s, s3)
+                                                        print('Decrypted text:')
+                                                        print(plaintext)
+                                                        return (s1, s, s3, plaintext)
+                print('\n')
+        print('DecipheringError: Something went wrong. Try updating the wordlist or lowering the tolerance.')
+        return None
+
 
 ### TESTS ###
 
@@ -305,18 +414,31 @@ ciphertext = ''
 with open(file_name, 'r') as dat:
         ciphertext = dat.read()
 
+# FIRST THE MEMORY-HEAVY VERSION
+# works great on longer words but is painfully slow on shorter ones
+
 # proof it works
 print('## TEST 1:')
+BreakGeffe2(ciphertext, ['CRYPTOGRAPHY'])
+
+# longer words
+print('## TEST 2:')
+BreakGeffe2(ciphertext)
+
+# SECOND THE TIME-HEAVY VERSION
+
+# proof it works
+print('## TEST 3:')
 BreakGeffe(ciphertext, ['CRYPTOGRAPHY'])
 
 
 # Interesting:
 # While attacking with the word 'prior' did not yield positive results,
-# the word 'the' succeded almost instantly despite being only 3 letters
-# long and not at all specific to the text.
-print('## TEST 2:')
+# the word 'the' succeded almost instantly (first occurance) despite being
+# only 3 letters long and not at all specific to the text.
+print('## TEST 4:')
 BreakGeffe(ciphertext, ['THE'] + WORDLIST)
 
 # if you like waiting
-print('## TEST 3:')
+print('## TEST 5:')
 BreakGeffe(ciphertext)
